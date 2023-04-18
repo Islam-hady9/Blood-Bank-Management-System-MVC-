9cd5dc53b6eaf6b9a23540e2c3db386b5209c33c
Islam Abd_Elhady <eslamabdo71239@gmail.com>
2023-04-18 16:09:49 +0200

    Modified

--- Blood Bank Management System/Controllers/HomeController.cs
+++ Blood Bank Management System/Controllers/HomeController.cs
@@ -4,47 +4,62 @@ using System.Diagnostics;
 
 namespace Blood_Bank_Management_System.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
-        public IActionResult Interface()
-        {
-            return View();
-        }
+		public IActionResult Interface()
+		{
+			return View();
+		}
 
-        public IActionResult Donors()
-        {
-            return View();
-        }
+		public IActionResult Donors()
+		{
+			return View();
+		}
 
-        public IActionResult Management()
-        {
-            return View();
-        }
-		
+		public IActionResult Management()
+		{
+			return View();
+		}
+
 		public IActionResult SearchBlood()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 
 		public IActionResult Hospitals()
+		{
+			return View();
+		}
+
+		public IActionResult RequestBlood()
+		{
+			return View();
+		}
+
+		public IActionResult SendRequest()
+		{
+			return View();
+		}
+
+		public IActionResult ViewRequests()
 		{
 			return View();
 		}

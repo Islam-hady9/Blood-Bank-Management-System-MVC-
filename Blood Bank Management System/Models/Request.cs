2992997218b1989017a8b4ec5a01db0c3a3f647a
Islam Abd_Elhady <eslamabdo71239@gmail.com>
2023-05-09 13:53:17 +0300

    Request Class Modified

--- Blood Bank Management System/Models/Request.cs
+++ Blood Bank Management System/Models/Request.cs
@@ -1,9 +1,7 @@
-﻿using System.Text.RegularExpressions;
-namespace Blood_Bank_Management_System.Models
+﻿namespace Blood_Bank_Management_System.Models
 {
     public class Request
     {
-
         private int requestID { get; set; }
         private bool requestStatus { get; set; } // accept or reject
         private float requestBloodQuantity { get; set; }
@@ -13,65 +11,59 @@ namespace Blood_Bank_Management_System.Models
         // Default Constructor...
         public Request()
         {
-            this.requestID = 0;
+            this.requestID = 1;
             this.requestStatus = true;
-            this.requestBloodQuantity = 500;
+            this.requestBloodQuantity = 5000;
             this.requestDate = DateTime.Now;
             this.requestBloodType = BloodType.A;
-
         }
         // Argument Constructor...
-        public Request(int requestID, bool requestStatus, float requestBloodQuantity, DateTime requestDate, BloodType requestBloodType)
+        public Request(int requestID, float requestBloodQuantity, DateTime requestDate, BloodType requestBloodType)
         {
-            if (requestID < 0)
+            if (requestID > 0)
             {
-                throw new ArgumentException("Invalid ID");
+                this.requestID = requestID;
             }
             else
             {
-                this.requestID = requestID;
-
+                throw new ArgumentException("Invalid ID");
             }
 
-            if (requestBloodQuantity < 5000)
+            if (requestBloodQuantity <= 5000.0f && requestBloodQuantity > 0.0f)
             {
-                this.requestBloodQuantity = 500;
+                this.requestBloodQuantity = 5000.0f;
                 this.requestStatus = true;
-
             }
             else
             {
                 this.requestStatus = false;
                 throw new Exception("The request is rejected");
             }
-
             this.requestDate = DateTime.Now;
             this.requestBloodType = BloodType.A;
-
         }
         //Setters...
         public void SetRequestID(int requestID)
         {
-            if (requestID < 0)
+            if (requestID > 0)
             {
-                throw new ArgumentException("Invalid ID");
+                this.requestID = requestID;
             }
             else
             {
-                this.requestID = requestID;
+                throw new ArgumentException("Invalid ID");
             }
         }
         public void SetRequestStatus(bool requestStatus)
         {
             this.requestStatus = requestStatus;
         }
-        public void SetBloodQuantity(float requestBloodQuantity)
+        public void SetRequestBloodQuantity(float requestBloodQuantity)
         {
-            if (requestBloodQuantity < 5000)
+            if (requestBloodQuantity <= 5000.0f && requestBloodQuantity > 0.0f)
             {
                 this.requestBloodQuantity = requestBloodQuantity;
                 this.requestStatus = true;
-
             }
             else
             {
@@ -94,19 +86,19 @@ namespace Blood_Bank_Management_System.Models
         }
         public float GetRequestBloodQuantity()
         {
-            return requestBloodQuantity;
+            return this.requestBloodQuantity;
         }
         public DateTime GetRequestDate()
         {
-            return requestDate;
+            return this.requestDate;
         }
         public BloodType GetRequestBloodType()
         {
-            return requestBloodType;
+            return this.requestBloodType;
         }
         public bool GetRequestStatus()
         {
-            return requestStatus;
+            return this.requestStatus;
         }
     }
 }
